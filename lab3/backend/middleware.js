dee016f2fd898f1a6350c84dd338a7b0882c06db
GabovYegor <gabov.egor2017@yandex.ru>
2019-09-30 20:00:21 +0300

    30.09.19 version

--- lab3/backend/middleware.js
+++ lab3/backend/middleware.js
@@ -1,8 +1,12 @@
 const DataBase = require('./DataBase/DataBase')
 const Picture = require('./DataBase/Picture')
 
-function loadMainPage(req, res) {
-    res.render('picturesListPage', {
+function loadAuctionSettingsPage(req, res){
+    res.render('auctionSettingsPage')
+}
+
+function loadPictureListPage(req, res) {
+    res.render('pictureListPage', {
         pictures: (new DataBase()).getPicturesMas()
     })
 }
@@ -15,17 +19,17 @@ function addPictureAction(req, res) {
     let imagePath = req.body.image
     if (!imagePath.length) { imagePath = 'default.png' }
     (new DataBase()).addPictureToDataBase(new Picture('/public/images/' + imagePath, req.body.title, req.body.author))
-    res.redirect('/')
+    res.redirect('/picturesListPage')
 }
 
 function removePictureAction(req, res){
     db = new DataBase()
     db.removePictureFromDataBaseByID(req.body.id)
-    res.redirect('/')
+    res.redirect('/picturesListPage')
 }
 
 function loadPictureCard(req, res){
-    res.render('pictureCard', {
+    res.render('pictureCardPage', {
         picture: (new DataBase()).getPictureByID(req.params.id)
     })
 }
@@ -36,10 +40,11 @@ function changePictureAction(req, res){
 }
 
 module.exports = {
-    loadMainPage: loadMainPage,
+    loadPictureListPage: loadPictureListPage,
     loadAddPicturePage: loadAddPicturePage,
     addPictureAction: addPictureAction,
     removePictureAction: removePictureAction,
     loadPictureCard: loadPictureCard,
-    changePictureAction: changePictureAction
+    changePictureAction: changePictureAction,
+    loadAuctionSettingsPage: loadAuctionSettingsPage
 }
