02a11b99b68668b8c1e511fff142ee266eea4b70
GabovYegor <gabov.egor2017@yandex.ru>
2019-10-04 04:27:55 +0300

    set up gulp

--- lab3/gulpfile.js
+++ lab3/gulpfile.js
@@ -4,21 +4,36 @@ const gulp = require('gulp')
 const less = require('gulp-less');
 const babel = require("gulp-babel");
 const pug = require('gulp-pug');
+const del = require("del");
 
-gulp.task('styles', function () {
+gulp.task('delStyle', function(){
+    return del('./public/styleProduction/*.css');
+})
+
+gulp.task('style', function () {
     return gulp.src('public/styles/*.less')
         .pipe(less())
-        .pipe(gulp.dest('public/styles/'))
+        .pipe(gulp.dest('public/stylesProduction/'))
 })
 
-gulp.task('js', function () {
-    return gulp.src('server.js')
-        .pipe(babel())
-        .pipe(gulp.dest('default'))
+gulp.task('updateStyle', gulp.series('delStyle', 'style'))
+
+gulp.task('delJS', function(){
+    return del('./public/JQueryCodeProduction/*.js');
 })
+
+gulp.task('js', function(){
+    return gulp.src('./public/JQueryCode/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('./public/JQueryCodeProduction/'))
+});
+
+gulp.task('updateJS', gulp.series('delJS', 'js'))
 
 gulp.task('views', function () {
     return gulp.src('views/*.pug')
         .pipe(pug())
-        .pipe(gulp.dest('default'))
+        .pipe(gulp.dest('viewsProduction'))
 })
+
+gulp.task('default', gulp.parallel('updateStyle', 'updateJS'));
