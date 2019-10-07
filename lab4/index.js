154a03bd046d1c15628f1677dae27f290317db7b
GabovYegor <gabov.egor2017@yandex.ru>
2019-10-07 11:34:33 +0300

    separate users

--- lab4/index.js
+++ lab4/index.js
@@ -8,7 +8,8 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+        console.log('message: ' + msg, socket.id);
+        socket.broadcast.emit('chat message', 'hello');
         io.emit('chat message', msg)
     });
 });
